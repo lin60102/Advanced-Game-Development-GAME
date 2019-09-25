b8ef55d5ffc9aedb99c890b08d5a8a4495ae11a9
CHAO CHENG LIN(Ken) <38403949+lin60102@users.noreply.github.com>
2019-09-25 17:32:05 -0400

    terrain

---
+++ Terrains/Assets/TerrainGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainGenerator : MonoBehaviour
+{
+    Terrain terrain;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        terrain = GetComponent<Terrain>();
+        float[,] heightmap = terrain.terrainData.GetHeights(0, 0, terrain.terrainData.heightmapHeight, terrain.terrainData.heightmapWidth);
+        for(int i=0; i<terrain.terrainData.heightmapHeight; i++)
+        {
+            for(int j=0; j < terrain.terrainData.heightmapWidth; j++)
+            {
+                float x = i / (float)terrain.terrainData.heightmapWidth;
+                float y = j / (float)terrain.terrainData.heightmapHeight;
+                heightmap[i, j] = Mathf.PerlinNoise(i, j);
+            }
+        }
+        terrain.terrainData.SetHeights(0, 0, heightmap);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
